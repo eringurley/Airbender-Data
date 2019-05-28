fed37cf93521b3dac35f9635cec11840e21314e0
Erin Gurley <erinmgurley@gmail.com>
2019-05-28 15:49:39 -0700

    built out initial App.js file with render and renderTemplate

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import Component from './Component.js';
+import Header from './Header.js';
+
+class App extends Component {
+    render() {
+        const dom = this.renderDOM();
+        return dom;
+    }
+
+    renderTemplate() {
+        return /*html*/ `
+    <div>
+        <main></main>
+    </div>
+    `;
+    }
+}
+
+export default App;
