4d87bf3bb82a50d4c30e5b5316322d6dfe44cc00
Erin Gurley <erinmgurley@gmail.com>
2019-05-28 19:21:13 -0700

    added a placeholder image in case no image present

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -3,11 +3,15 @@ import Component from './Component.js';
 class CharacterItem extends Component {
     renderTemplate() {
         const character = this.props.character;
+        if(!character.photoURL) {
+            character.photoURL = '../../assets/placeholder.jpg';
+        }
+
         return /*html*/ `
-        <li class="character-item">
-            <h1>${character.name}</h1>
-            <img src="${character.photoURL}" alt="${character.name}">
-        </li>
+            <li class="character-item">
+                <h1>${character.name}</h1>
+                <img src="${character.photoUrl}" alt="${character.name}">
+            </li>
         `;
     }
 }
