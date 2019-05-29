09411717905f030c8391b4a4ef34ae971b0653e2
Erin Gurley <erinmgurley@gmail.com>
2019-05-28 17:20:26 -0700

    added some static character data to test

---
+++ test/characters.js
@@ -0,0 +1,16 @@
+const characters = [
+    {
+        name: '46th Earth King',
+        photoUrl: 'https://vignette.wikia.nocookie.net/avatar/images/5/51/46th_Earth_King.png/revision/latest?cb=20130627160441'
+    },
+    {
+        name: 'Aang',
+        photoUrl: 'https://vignette.wikia.nocookie.net/avatar/images/a/ae/Aang_at_Jasmine_Dragon.png/revision/latest?cb=20130612174003'
+    },
+    {
+        name: 'Aang (games)',
+        photoUrl: 'https://vignette.wikia.nocookie.net/avatar/images/2/21/Into_The_Inferno_Aang.png/revision/latest?cb=20131009060746'
+    },
+];
+
+export default characters;
