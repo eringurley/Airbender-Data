9e5104f0cf878599e8185f6ddd3c50ac93439275
Erin Gurley <erinmgurley@gmail.com>
2019-05-28 21:29:53 -0700

    fixed the image problem but broke a test

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -3,16 +3,20 @@ import Component from './Component.js';
 class CharacterItem extends Component {
     renderTemplate() {
         const character = this.props.character;
-        
-        if(!character.photoURL) {
-            character.photoURL = '../../../assets/placeholder.jpg';
-        }
-        return /*html*/ `
+        if(character.photoUrl) {
+            return /*html*/ `
             <li class="character-item">
-                <h1>${character.name}</h1>
-                <img src="${character.photoURL}" alt="${character.name}">
+                <h2>${character.name}</h2>
+                <img src="${character.photoUrl}" alt="${character.name}">
             </li>
         `;
+        }
+        return /*html*/ `
+            <li>
+                <h2>${character.name}</h2>
+                <img src="./assets/placeholder.jpg" id="placeholder" alt="${character.name}">
+            </li>
+        `;  
     }
 }
 
