6268f406c68129ed9d5603c8c009189fbe7d0b6e
Erin Gurley <erinmgurley@gmail.com>
2019-05-29 09:03:37 -0700

    changed photoUrl

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -3,19 +3,18 @@ import Component from './Component.js';
 class CharacterItem extends Component {
     renderTemplate() {
         const character = this.props.character;
-        if(character.photoUrl) {
-            return /*html*/ `
+        if(!character.photoUrl) {
+            character.photoUrl = './assets/placeholder.jpg';
+            // <li class="character-item">
+            //     <h2>${character.name}</h2>
+            //     <img src="${character.photoUrl}" alt="${character.name}">
+            // </li>
+        }
+        // else if(!character.photoUrl)
+        return /*html*/ `
             <li class="character-item">
                 <h2>${character.name}</h2>
-                <img src="${character.photoURL}" alt="${character.name}">
-            </li>
-        `;
-        }
-        // else if(!character.photoURL)
-        return /*html*/ `
-            <li>
-                <h2>${character.name}</h2>
-                <img src="./assets/placeholder.jpg" id="placeholder" alt="${character.name}">
+                <img src="${character.photoURL}" id="placeholder" alt="${character.name}">
             </li>
         `;  
     }
