24fbfcab2b8de200e7d89765155616e3656a7e46
Erin Gurley <erinmgurley@gmail.com>
2019-05-29 17:18:11 -0700

    added enemy and ally links and set them to key, value in the renderTemplate

--- src/components/CharacterItem.js
+++ src/components/CharacterItem.js
@@ -3,6 +3,13 @@ import Component from './Component.js';
 class CharacterItem extends Component {
     renderTemplate() {
         const character = this.props.character;
+
+        const enemyLink = new URLSearchParams();
+        enemyLink.set('enemies', character.name);
+
+        const allyLink = new URLSearchParams();
+        allyLink.set('allies', character.name);
+
         if(!character.photoUrl) {
             character.photoUrl = './assets/placeholder.jpg';
         }
@@ -10,6 +17,8 @@ class CharacterItem extends Component {
             <li class="character-item">
                 <h2>${character.name}</h2>
                 <img src="${character.photoUrl}" alt="${character.name}">
+                <a href="#${enemyLink.toString()}">Enemies</a>
+                <a href="#${allyLink.toString()}">Allies</a>
             </li>
         `;  
     }
