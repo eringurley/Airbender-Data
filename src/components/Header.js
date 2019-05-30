2fa00a948cb1eb778baf50c1029dd97806fce763
Erin Gurley <erinmgurley@gmail.com>
2019-05-29 17:20:25 -0700

    added the show all characters

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,8 +3,10 @@ import Component from './Component.js';
 class Header extends Component {
     renderTemplate() {
         return /*html*/ `
+        <header>
+        <a href="">Back Home</a>
         <h1>The Last Airbender</h1>
-
+        </header>
         `;
     }
 }
